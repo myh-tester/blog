a3c32c2e514e7059b0ee48431f0675d871dfba4c
myh <m472322997@163.com>
2023-10-30 23:43:22 +0800

    docs: 配置修改

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,9 +1,10 @@
 import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 
+const GITHUB = 'https://github.com/myh-tester/blog'
+
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  base: '/blog/',
   title: 'Blog',
   description: 'Tester learning document collection',
   icon: "/rspress-icon.png",
@@ -17,17 +18,17 @@ export default defineConfig({
     hideNavbar: 'auto',
     editLink: {
       docRepoBaseUrl:
-        'https://github.com/M-sole-0312/blog/tree/main/packages/document/docs',
+        'https://github.com/myh-tester/blog/tree/main/docs',
       text: '📝 在 GitHub 上编辑此页',
     },
     footer: {
-      message: '© 2023 Люби себя. All Rights Reserved.',
+      message: `Люби себя 的博客，版权所有 © 2023-${new Date().getFullYear()}`,
     },
     socialLinks: [
       {
         icon: 'github',
         mode: 'link',
-        content: 'https://github.com/M-sole-0312/blog',
+        content: GITHUB,
       }
     ],
   },
